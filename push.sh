8b75feaacba5e20e154c994a8c2670d5f8877694
Thomthomtt <thomazgt39@gmail.com>
2023-09-06 19:07:30 +0200

    Automatizado pelo script

--- push.sh
+++ push.sh
@@ -1,14 +1,4 @@
-#!bin/bash
-
-if ! command -v git &>/dev/null; then
-echo "O git nao esta instalado. Instale-o antes de usar este script."
-exit 1
-fi
-current_dir=$(pwd)
-
-current_branch=$(git branch | grep '*'| cut -d '' -f 2)
-
-remote_url=$(git remote get-url origin)
+#!bin/bash 
 
 if git status -s | grep -q '^M'; then
 echo "Ha alteracoes nao commitadas. Commite-as antes de usar este script."
